0fe5f84e1c4de368e5b6605f7c52150b93e635de
luke-h1 <luke.howsam@yahoo.com>
2024-09-13 10:33:18 +0100

    chore: fix onPageChange

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -6,6 +6,7 @@ import storageService from "./services/localStorageService";
 function App() {
   const [text, setText] = useState("");
   const [totalPages, setTotalPages] = useState<number[]>([]);
+  const [searchHistory, setSearchHistory] = useState<string[]>([]);
 
   const [searchItems, setSearchItems] = useState<SearchResponse[]>([]);
   const [currPage, setCurrPage] = useState<number>(1);
@@ -13,10 +14,9 @@ function App() {
   const onSubmit = async (pageNum?: number) => {
     const { data, paging } = await searchService.search(text, pageNum);
 
-    // setTotalPages([...totalPages]);
-
     // paging.totalPages is a number of total pages
-    // turn it into an array of numbers
+    // turn it into an array of numbers so we can map over it and render a button for each page
+    // for the length of totalPages, create an array of numbers starting from 1
     const tp = Array.from({ length: paging.totalPages }, (_, i) => i + 1);
 
     setCurrPage(paging.page);
@@ -28,9 +28,18 @@ function App() {
     if (history.length >= 5) {
       history.splice(0, 1);
       storageService.setSync("history", history);
+      setSearchHistory(history);
     }
 
     storageService.setSync("history", [...history, text]);
+    setSearchHistory([...history, text]);
+  };
+
+  const onPageChange = async (page: number) => {
+    setCurrPage(page);
+    const { data, paging } = await searchService.search(text, page);
+    setSearchItems(data);
+    setCurrPage(paging.page);
   };
 
   return (
@@ -68,6 +77,15 @@ function App() {
           {searchItems && searchItems.map((item) => <li>{item.title}</li>)}
         </ul>
       </div>
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <div>
+        <h3>Search History</h3>
+        <ul>{searchHistory && searchHistory.map((hist) => <li>{hist}</li>)}</ul>
+      </div>
 
       <br />
       <br />
@@ -78,7 +96,7 @@ function App() {
       <div style={{ display: "flex", flexDirection: "row" }}>
         {totalPages.map((page) => (
           <button
-            onClick={() => onSubmit(page)}
+            onClick={() => onPageChange(page)}
             style={{
               margin: "0.5rem",
               color: page === currPage ? "red" : "black",
